3190aca3071434af5925f565f52548a59c181a99
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-06-06 09:06:04 +0300

    Add challange 31

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 "use strict";
+
+// 31. reverse
+/* 
+Write a recursive function called reverse which accepts a string and returns a new string in reverse
+*/
+function reverse(str) {
+    if (str.length === 0) return "";
+
+    return str.slice(-1) + reverse(str.slice(0, -1));
+}
+
+// reverse('awesome') // 'emosewa'
+// reverse('rithmschool') // 'loohcsmhtir
+console.log(reverse("awesome"));
+
+// tail recursion
+function reverse2(str, revWord = "") {
+    if (str.length === 0) return revWord;
+
+    return reverse2(str.slice(0, -1), revWord + str.slice(-1));
+}
+console.log(reverse2("awesome"));
