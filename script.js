85b936d7a185dead560014922e4153ad3ecbb084
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-06-07 10:14:54 +0300

    Add challange 32

--- script.js
+++ script.js
@@ -21,3 +21,23 @@ function reverse2(str, revWord = "") {
     return reverse2(str.slice(0, -1), revWord + str.slice(-1));
 }
 console.log(reverse2("awesome"));
+
+// /////////////////////////
+// 32. isPalindrome
+/* 
+Write a recursive function called isPalindrome  which returns true if the string passed to it is a palindrome (reads the same forward and backward). Otherwise it returns false.
+*/
+// isPalindrome('awesome') // false
+// isPalindrome('foobar') // false
+// isPalindrome('tacocat') // true
+// isPalindrome('amanaplanacanalpanama') // true
+// isPalindrome('amanaplanacanalpandemonium') // false
+
+function isPalindrome(str) {
+    if (str.length === 0) return true;
+
+    return str[0] === str.slice(-1) && isPalindrome(str.slice(1, -1));
+}
+
+// console.log(isPalindrome("tacocat"));
+// console.log(isPalindrome("amanaplanacanalpanama"));
