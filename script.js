ed12ce2ab77d8dc4a3688dfd2c883e870fe734dd
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-06-10 15:09:10 +0300

    Add challange 34

--- script.js
+++ script.js
@@ -64,3 +64,42 @@ function someRecursive(arr, callback) {
 }
 const isOdd = (val) => val % 2 !== 0;
 // console.log(someRecursive([1, 2, 3, 4], isOdd));
+
+///////////////////////////////////////
+// 34. flatten
+/* 
+Write a recursive function called flatten  which accepts an array of arrays and returns a new array with all values flattened.
+*/
+// flatten([1, 2, 3, [4, 5] ]) // [1, 2, 3, 4, 5]
+// flatten([1, [2, [3, 4], [[5]]]]) // [1, 2, 3, 4, 5]
+// flatten([[1],[2],[3]]) // [1,2,3]
+// flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]) // [1,2,3
+
+function flatten(arr, flat = []) {
+    while (typeof arr[0] !== "object" && arr[0]) {
+        flat.push(arr.shift());
+    }
+
+    if (arr.length === 0) return flat;
+    else return flatten([...arr[0], ...arr.slice(1)], flat);
+}
+
+// function flatten(arr) {}
+
+console.log(flatten([[[[1], [[[2]]], [[[[[[[3]]]]]]]]]]));
+
+console.log(flatten([1, [2, [3, 4], [[5]]]]));
+// console.log(typeof [1]);
+
+// A different solution
+function flatten2(oldArr) {
+    var newArr = [];
+    for (var i = 0; i < oldArr.length; i++) {
+        if (Array.isArray(oldArr[i])) {
+            newArr = newArr.concat(flatten2(oldArr[i]));
+        } else {
+            newArr.push(oldArr[i]);
+        }
+    }
+    return newArr;
+}
