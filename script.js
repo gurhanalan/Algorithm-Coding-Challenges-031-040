a86277350b8f86ac7099484ad341511724243ac7
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-11-28 08:31:37 +0300

    Add Challange

--- script.js
+++ script.js
@@ -124,3 +124,67 @@ function capitalizeFirst(arr, cap = []) {
 // capitalizeFirst(['car','taco','banana']); // ['Car','Taco','Banana']
 
 console.log(capitalizeFirst(["car", "taco", "banana"]));
+
+// ///////////////////////
+//36. nestedEvenSum
+/* 
+Write a recursive function called nestedEvenSum . Return the sum of all even numbers in an object which may contain nested objects.
+
+var obj1 = {
+  outer: 2,
+  obj: {
+    inner: 2,
+    otherObj: {
+      superInner: 2,
+      notANumber: true,
+      alsoNotANumber: "yup"
+    }
+  }
+}
+
+var obj2 = {
+  a: 2,
+  b: {b: 2, bb: {b: 3, bb: {b: 2}}},
+  c: {c: {c: 2}, cc: 'ball', ccc: 5},
+  d: 1,
+  e: {e: {e: 2}, ee: 'car'}
+};
+
+nestedEvenSum(obj1); // 6
+nestedEvenSum(obj2); // 10
+*/
+
+function nestedEvenSum(obj) {
+    let total = 0;
+    for (let el in obj) {
+        if (typeof obj[el] === "number" && obj[el] % 2 === 0) {
+            total += obj[el];
+        } else if (typeof obj[el] === "object") {
+            total += nestedEvenSum(obj[el]);
+        }
+    }
+    return total;
+}
+
+var obj1 = {
+    outer: 2,
+    obj: {
+        inner: 2,
+        otherObj: {
+            superInner: 2,
+            notANumber: true,
+            alsoNotANumber: "yup",
+        },
+    },
+};
+
+console.log(nestedEvenSum(obj1));
+var obj2 = {
+    a: 2,
+    b: { b: 2, bb: { b: 3, bb: { b: 2 } } },
+    c: { c: { c: 2 }, cc: "ball", ccc: 5 },
+    d: 1,
+    e: { e: { e: 2 }, ee: "car" },
+};
+
+console.log(nestedEvenSum(obj2));
