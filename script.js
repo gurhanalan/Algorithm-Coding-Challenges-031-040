3c7ff12c0b774feb1c5e0b8acb9bdf3de6151dca
Gurhan <83542792+gurhanalan@users.noreply.github.com>
2021-12-01 08:37:05 +0300

    Add Challange 39

--- script.js
+++ script.js
@@ -293,3 +293,56 @@ let obj = {
 };
 
 console.log(stringifyNumbers3(obj));
+
+// /////////////////////////////////////////
+//39. collectStrings
+/* 
+Write a function called collectStrings  which accepts an object and returns an array of all the values in the object that have a typeof string
+
+const obj = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz"
+                    }
+                }
+            }
+        }
+    }
+}
+
+collectStrings(obj) // ["foo", "bar", "baz"])
+*/
+function collectStrings(obj) {
+    let total = [];
+    for (let el in obj) {
+        if (typeof obj[el] === "string") {
+            total.push(obj[el]);
+        } else if (typeof obj[el] === "object") {
+            total.push(...collectStrings(obj[el]));
+        }
+    }
+    return total;
+}
+
+const obj5 = {
+    stuff: "foo",
+    data: {
+        val: {
+            thing: {
+                info: "bar",
+                moreInfo: {
+                    evenMoreInfo: {
+                        weMadeIt: "baz",
+                    },
+                },
+            },
+        },
+    },
+};
+
+console.log(collectStrings(obj5));
